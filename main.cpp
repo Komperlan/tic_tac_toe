91ad27fa8a7964de818e05a2eed308d07a6350ed
Komperlan <legionerjustin@gmail.com>
2023-09-21 12:35:30 +0300

    Create Otput_game_field func

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+
 int main(){
-    std::cout<<"GG";
+    char gF[3][3];
+    for(int x = 0; x<3; x++){
+        for(int y = 0; y<3; y++){
+            gF[x][y] = '_';
+        }
+    }
+    while(CheckGameState(gF) == "Game`s Continue"){
+        PlayerTurn(gF);
+        if(CheckGameState(gF) == "Game`s Continue"){
+            AIturn(gF);
+        }
+        Output_game_field(gF);
+    }
+    std:: cout<< CheckGameState(gF);
 }
